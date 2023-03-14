55bfad595d8c0ce5174825f3956fba5290926f2f
Danny Yu <tiramisu@Dannys-MBP.home>
2023-03-14 19:25:03 -0400

    profile

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,14 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  netScore: Number,
+  jobScore: Number,
+  skillScore: Number,
+  badge: [{ type: Schema.Types.ObjectId, ref: "Badge" }],
+  skillsUnlock: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+  networkMade: [{ type: Schema.Types.ObjectId, ref: "Network" }],
+  jobApplied: [{ type: Schema.Types.ObjectId, ref: "Job" }],
+
 },{
   timestamps: true,
 })
